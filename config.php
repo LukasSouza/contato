18f8d79eef6860dd63df8398209de394241439a6
LukasSouza <lukas_santos15@hotmail.com>
2020-09-27 19:39:59 -0300

    first commit

--- config.php
+++ config.php
@@ -1,10 +1,7 @@
 <?php
-    // ini_set('auth_username', 'lukas.santos15@gmail.com');
-    // ini_set('auth_password', '3216549870Xx.');
-    // ini_set('sendmail_from', 'lukas.santos15@gmail.com');
+
     ini_set('SMTP','smtp.gmail.com');
     ini_set('smtp_port',587);
-    // ini_set('smtp_ssl','ssl');
 
     define("EMAIL", "lsouza2@outlook.com");
 
